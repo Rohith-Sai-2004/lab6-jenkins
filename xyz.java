7e99fb924856e5cf7e1fef5593c2c7908212c952
Rohith Sai Reddy Thokala <rohithsaireddythokala@gmail.com>
2026-02-16 16:04:16 +0530

    Update xyz.java to read integers from user input
    Refactored code to take user input for variables a and b.

--- xyz.java
+++ xyz.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class xyz {
     public static void main(String[] args) {
         System.out.println("Enter a number");
-        int a=10,b=30;
+        Scanner sc = new Scanner(System.in); 
+        int a,b;
+        if(sc.hasNextInt()){
+            a = sc.nextInt();
+        }
+        System.out.println("Enter a number");
+        if(sc.hasNextInt()){
+            b = sc.nextInt();
+        }
         System.out.println("Sum: "+(a+b));
     }
 }   
